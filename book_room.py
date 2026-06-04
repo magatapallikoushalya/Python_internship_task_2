f70f3dbec78d68e72c8aeeeb5b8157e0fefd4e2e
koushalya <koushalyamagatapalli@gmail.com>
2026-06-04 10:50:31 +0530

    Update book_room.py

--- book_room.py
+++ book_room.py
@@ -1,5 +1,9 @@
+rooms = {101: "Available",102: "Available",103: "Available"}
 def book_room():
-    room = input("Enter room number: ")
-    print("Room", room, "booked successfully")
-
+    room_no = int(input("Enter Room Number: "))
+    if room_no in rooms and rooms[room_no] == "Available":
+        rooms[room_no] = "Booked"
+        print("Room Booked Successfully")
+    else:
+        print("Room Not Available")
 book_room()
