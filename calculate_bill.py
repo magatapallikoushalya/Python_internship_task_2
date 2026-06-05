9e63806acff54bb3edd6c98597905cb98f66a7af
koushalya <koushalyamagatapalli@gmail.com>
2026-06-05 10:16:25 +0530

    Add function to calculate hotel bill based on days

---
+++ calculate_bill.py
@@ -0,0 +1,6 @@
+def calculate_bill():
+    days = int(input("Enter Number of Days: "))
+    room_charge = 1000
+    total_bill = days * room_charge
+    print("Total Bill =", total_bill)
+calculate_bill()
